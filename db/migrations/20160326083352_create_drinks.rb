c67f81b6133570bbff243b2e197e7a5e61f464ed
blooper05 <legend.of.blooper@gmail.com>
2016-03-26 17:35:40 +0900

    hanami db migrate

--- db/migrations/20160326083352_create_drinks.rb
+++ db/migrations/20160326083352_create_drinks.rb
@@ -1,4 +1,9 @@
 Hanami::Model.migration do
   change do
+    create_table :drinks do
+      primary_key :id
+      column :name,    String, null: false
+      column :country, String, null: false
+    end
   end
 end
